593d932a7c67bf94f82bae42a118cdd6478f4a23
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-19 14:12:07 +0100

    counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,30 @@
 import "./Counter.scss";
 import minus from "../../assets/images/minus.svg";
 import plus from "../../assets/images/plus.svg";
+import { useState } from "react";
 
 const Counter = () => {
+    const [ticket, setTicket] = useState(0);
+    const handleMinus = () => {
+        if (ticket === 0) {
+            setTicket(0);
+        }else{
+            setTicket(ticket - 1);
+        }
+    }
+
+    const handlePlus = () => {
+        setTicket(ticket + 1);
+    }
+
+
     return(
     <section className="counterCard">
         <h2>TICKET</h2>
-            <h3>0</h3>
+            <h3>{ticket}</h3>
             <div className="button">
-                <img src={minus} alt={minus}  />
-                <img src={plus} alt={plus}  />
+                <img onClick={handleMinus} src={minus} alt={minus}  />
+                <img onClick={handlePlus} src={plus} alt={plus}  />
             </div>
     </section >
 )}
